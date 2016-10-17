bdcd3bd91361d35d8ca72af3fadadaf5dc8a4636
89191482577 <89191482577@bk.ru>
2016-10-17 17:34:48 +0500

    Create Symvols.java

---
+++ symvols/Symvols.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package symvols;
+
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+ 
+public class Symvols {
+    private JTextField txtField;
+    private JLabel txtLabel;
+    private JLabel kmLabel;
+    private JButton calcButton;
+    private JLabel resultLabel;
+    private JFrame mainFrame;
+    
+    Symvols() {
+    
+    }
+    
+}
